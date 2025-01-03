1619e1f7c4de6daf8d85c3e51852fa681fa5227c
Sergei Babrakov <s.a.babrakov@gmail.com>
2025-01-03 09:55:52 +0100

    Score Persistence

--- singletons/score_manager.gd
+++ singletons/score_manager.gd
@@ -4,10 +4,11 @@ var _level_selected: int = 1
 var _level_scores: Dictionary = {}
 
 const DEFAULT_SCORE: int = 1000
+const SCORES_PATH = "user://animals.json"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	load_from_disk()
 
 func set_level_selected(ls: int) -> void:
 	_level_selected = ls
@@ -23,7 +24,21 @@ func set_score_for_level(score: int, level: String) -> void:
 	check_and_add(level)
 	if (_level_scores[level] > score):
 		_level_scores[level] = score
+		save_to_disk()
 
 func get_best_score(level: String) -> int:
 	check_and_add(level)
 	return _level_scores[level]
+
+func save_to_disk():
+	var file = FileAccess.open(SCORES_PATH, FileAccess.WRITE)
+	var score_json_str = JSON.stringify(_level_scores)
+	file.store_string(score_json_str)
+
+func load_from_disk():
+	var file = FileAccess.open(SCORES_PATH, FileAccess.READ)
+	if file == null:
+		save_to_disk()
+	else:
+		var data = file.get_as_text()
+		_level_scores = JSON.parse_string(data)
